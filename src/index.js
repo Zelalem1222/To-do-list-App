d1a598ac903fb0ed0c8d6b18d5904fe25a02fc98
Zelalem1222 <zelalemfissha394@gmail.com>
2022-06-07 14:04:57 +0300

    set up the project with webpack

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import './style.css';
+const listContainer = document.getElementById('container');
+
+listContainer.innerHTML += `
+ <h1>todays to do list</h1>   
+<div>
+   <ul>
+   <li>hey</li>
+   <li>how are you</li>
+   <li>good</li>
+   </ul>
+   </div>
+`
