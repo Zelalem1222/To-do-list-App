eef20376253b2b15676fed492ef82521e0425a43
Zelalem1222 <zelalemfissha394@gmail.com>
2022-06-08 16:12:13 +0300

    Update add function of the app

--- src/index.js
+++ src/index.js
@@ -1,33 +1,44 @@
 import './style.css';
 
 const todolists = document.querySelector('.todo-container');
+const textInput = document.querySelector('input')
+class Object{
+  constructor(index , description , completed){
+      this.index = index;
+      this.description = description;
+      this.completed = completed;
+  }
+}
 
-const listArray = [
-  {
-    index: 1,
-    description: 'wash dishes',
-    completed: false,
-  },
-  {
-    index: 2,
-    description: 'completed all projects',
-    completed: true,
-  },
-];
+const arr = [];
 
-const displayLists = () => {
-  listArray.forEach((list) => {
+const addTodo = (todoValue)=> {
     const todoContainer = document.createElement('div');
     todoContainer.className = 'list border-bottom';
     todoContainer.innerHTML += `
           <div>
           <input type="checkbox" class="check">
-          <span>${list.description}</span>
+          <span>${todoValue}</span>
           </div>
           <i class="fa-solid fa-ellipsis-vertical"></i>
           `;
     todolists.append(todoContainer);
-  });
-};
+    const checkbox = document.querySelectorAll('.check');
+    for (let i=0; i < checkbox.length; i++){
+        checkbox[i].addEventListener('click' , ()=> {
+        checkbox[i].parentElement.classList.toggle('check-box')
+        })
+    }
+}
 
-displayLists();
+
+
+textInput.addEventListener('keypress' , (e)=> {
+  if(e.key === 'Enter' && textInput.value) {
+    e.preventDefault();  
+    addTodo(textInput.value);
+      textInput.value = '';
+  }
+})
+
+
