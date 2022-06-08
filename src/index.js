e1aa7e1da91d4f6f193c4d9f0f9a06078fbd295d
Zelalem1222 <zelalemfissha394@gmail.com>
2022-06-08 07:40:12 +0300

    update the app

--- src/index.js
+++ src/index.js
@@ -1,13 +1,46 @@
 import './style.css';
 const listContainer = document.getElementById('container');
+const todoInput = document.querySelector('input');
+const todolists = document.querySelector('.todo-container');
+const clearButton = document.querySelector('button');
 
-listContainer.innerHTML += `
- <h1>todays to do list</h1>   
-<div>
-   <ul>
-   <li>hey</li>
-   <li>how are you</li>
-   <li>good</li>
-   </ul>
-   </div>
-`
+class listObject  {
+    constructor(description , completed , index){
+        this.description = description;
+        this.completed = completed;
+        this.index = index;
+    }
+}
+
+const listArray = [
+   {
+       index : 1,
+       description : 'wash dishes',
+       completed : false
+   },
+   {
+       index: 2,
+       description: 'completed all projects',
+       completed: true
+   }
+];
+
+const lists = new listObject();
+const displayLists = () => {
+    listArray.forEach(list => {
+        const todoContainer = document.createElement('div');
+          todoContainer.className = 'list border-bottom';
+          todoContainer.innerHTML += `
+          <div>
+          <input type="checkbox" class="check">
+          <span>${list.description}</span>
+          </div>
+          <i class="fa-solid fa-ellipsis-vertical"></i>
+          `
+          todolists.append(todoContainer);
+    })
+}
+
+displayLists();
+
+
