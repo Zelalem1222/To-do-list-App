8ae899e6b878a0de9352044d62d2bc1c94ec1a09
Zelalem1222 <zelalemfissha394@gmail.com>
2022-06-17 13:15:17 -0400

    add functionalities

--- src/modules/functions.js
+++ src/modules/functions.js
@@ -9,4 +9,24 @@ export const addTodo = (description, tasks) => {
   return newTasks;
 };
 
-export const deleteTodo = (index, tasks) => tasks.filter((task) => task.index !== index);
+export const deleteTodo = (index, tasks) =>
+  tasks.filter((task) => task.index !== index);
+
+export const editTodo = (tasks, description, index) => {
+  const newTasks = tasks;
+  const task = newTasks.find((t) => t.index === index);
+  task.description = description;
+  newTasks[index] = task;
+  return newTasks;
+};
+
+export const updateTodoStatus = (tasks, index) => {
+  const newTasks = tasks;
+  const task = newTasks.find((t) => t.index === index);
+  task.completed = !task.completed;
+  newTasks[index] = task;
+  return newTasks;
+};
+
+export const removeAllCompleted = (tasks) =>
+  tasks.filter((task) => !task.completed);
