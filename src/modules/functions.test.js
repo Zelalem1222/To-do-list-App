8c42703ab44d927227ec9e23f2be5a50147f618e
Zelalem1222 <zelalemfissha394@gmail.com>
2022-06-17 13:15:50 -0400

    Add tests

--- src/modules/functions.test.js
+++ src/modules/functions.test.js
@@ -1,11 +1,19 @@
-import { addTodo, deleteTodo } from './functions';
+import {
+  addTodo,
+  deleteTodo,
+  removeAllCompleted,
+  editTodo,
+  updateTodoStatus,
+} from "./functions";
+import tasksMock from '../../__mocks__/mock.json';
 
-let tasks = [];
-describe('add and remove test', () => {
-  test('add todo', () => {
+
+let tasks = tasksMock;
+describe("add and remove test", () => {
+  test("add todo", () => {
     const { length } = tasks;
     const newTask = {
-      description: 'new task',
+      description: "new task",
       completed: false,
       index: 0,
     };
@@ -14,10 +22,26 @@ describe('add and remove test', () => {
     expect(tasks.length).toBe(length + 1);
   });
 
-  test('delete todo', () => {
+  test("delete todo", () => {
     const { length } = tasks;
     tasks = deleteTodo(0, tasks);
 
     expect(tasks.length).toBe(length - 1);
+  });
+
+  test("Remove all", () => {
+    const lengthArray = tasks.length;
+    tasks = removeAllCompleted(tasks);
+    expect(tasks).toHaveLength(lengthArray - 1);
+  });
+
+  test("edit todo", () => {
+    tasks = editTodo(tasks, "edited task", 1);
+    expect(tasks[1].description).toBe("edited task");
+  });
+
+  test("update todo status", () => {
+    tasks = updateTodoStatus(tasks, 1);
+    expect(tasks[1].completed).toBe(true);
   });
 });
